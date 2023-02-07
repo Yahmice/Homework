5cee9e92532f629813b59aefc183152595b419b9
Yahmice <83534639+Yahmice@users.noreply.github.com>
2023-02-07 22:12:41 +0500

    Update Homework #1.py

--- Homework #1.py
+++ Homework #1.py
@@ -7,11 +7,6 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
         
-    def avg(self):
-        avg = []
-        for value in self.grades.values():
-            avg.extend(value)
-        return self.grades.values()
         
         
     def rate_lecturer(self, lecturer, course, grade):
@@ -23,15 +18,17 @@ class Student:
         else:
             return 'Ошибка'
 
+    def avg(self):
+        avg = []
+        for value in self.grades.values():
+            avg.extend(value)
+        return sum(avg) / len(avg)
+
     def __str__(self):
         res = f'Имя: {self.name}, Фамилия: {self.surname}, Средняя оценка за домашние задания: {self.avg()}, Курсы в процессе изучения: {self.courses_in_progress}, Завершенные курсы: {self.finished_courses}'
         return res
        
 
-    
-
-
-    
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -48,6 +45,12 @@ class Lecturer(Mentor):
         for key, sub_dict in self.grades.items():
             res = sum(sub_dict) / len(sub_dict)
         return res
+
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            print('Not a lecturer!')
+            return
+        return self.average < other.average
 
     def __str__(self):
         res = f'Имя: {self.name}, Фамилия: {self.surname}, Средняя оценка за лекции: {self.average()}'
@@ -66,24 +69,18 @@ class Reviewer(Mentor):
     def __str__(self):
         res = f'Имя: {self.name}, Фамилия: {self.surname}'
         return res
-        
- 
-# best_student = Student('Ruoy', 'Eman', 'your_gender')
-# 
- 
-# cool_mentor = Mentor('Some', 'Buddy')
-# cool_mentor.courses_attached += ['Python']
- 
-# cool_mentor.rate_hw(best_student, 'Python', 10)
-# cool_mentor.rate_hw(best_student, 'Python', 10)
-# cool_mentor.rate_hw(best_student, 'Python', 10)
- 
-# print(best_student.grades)
+
+#Практики
 some_reviwer = Reviewer('Some', 'Body')
 first_reviwer = Reviewer('Some', 'Body')
 second_reviwer = Reviewer('Some', 'Body')
 third_reviwer = Reviewer('Some', 'Body')
+
+#Лекторы
 some_lecturer = Lecturer('Some', 'One')
+first_lecturer = Lecturer('Tim', 'Drake')
+second_lecturer = Lecturer('Dick', 'Grayson')
+third_lecturer = Lecturer('Jason', 'Todd')
 
 #Студенты
 best_student = Student('Shinra', 'Tensei', 'male')
@@ -98,6 +95,12 @@ middle_student.courses_in_progress += ['Python']
 best_student.courses_in_progress += ['Python']
 some_lecturer.courses_attached += ['Python']
 
+some_reviwer.courses_attached += ['Python']
+first_reviwer.courses_attached += ['Python']
+second_reviwer.courses_attached += ['Python']
+third_reviwer.courses_attached += ['Python']
+
+
 #Пройденные курсы 
 best_student.finished_courses += ['Начало программирования']
 
@@ -107,6 +110,23 @@ middle_student.rate_lecturer(some_lecturer, 'Python', 7.5)
 average_student.rate_lecturer(some_lecturer, 'Python', 9)
 bad_student.rate_lecturer(some_lecturer, 'Python', 4.6)
 
+best_student.rate_lecturer(first_lecturer, 'Python', 6.7)
+middle_student.rate_lecturer(first_lecturer, 'Python', 4.5)
+average_student.rate_lecturer(first_lecturer, 'Python', 3.3)
+bad_student.rate_lecturer(first_lecturer, 'Python', 3)
+
+best_student.rate_lecturer(second_lecturer, 'Python', 10)
+middle_student.rate_lecturer(second_lecturer, 'Python', 9.9)
+average_student.rate_lecturer(second_lecturer, 'Python', 8.8)
+bad_student.rate_lecturer(second_lecturer, 'Python', 2.6)
+
+best_student.rate_lecturer(third_lecturer, 'Python', 7.8)
+middle_student.rate_lecturer(third_lecturer, 'Python', 7.7)
+average_student.rate_lecturer(third_lecturer, 'Python', 6.3)
+bad_student.rate_lecturer(third_lecturer, 'Python', 2)
+
+
+
 #Оценки студентов
 some_reviwer.rate_hw(best_student, 'Python', 9)
 first_reviwer.rate_hw(best_student, 'Python', 8.7)
@@ -114,5 +134,6 @@ second_reviwer.rate_hw(best_student, 'Python', 10)
 third_reviwer.rate_hw(best_student, 'Python', 8.8)
 
 
-print(best_student)
+print(some_lecturer < first_lecturer)
+
 
