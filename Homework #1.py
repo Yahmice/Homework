d78515d80ea0a861cd9d363b0a7c35e0a52e127f
Yahmice <83534639+Yahmice@users.noreply.github.com>
2023-02-07 20:04:22 +0500

    Update Homework #1.py

--- Homework #1.py
+++ Homework #1.py
@@ -16,12 +16,24 @@ class Student:
         else:
             return 'Ошибка'
         
+    def avg(self):
+        for key, values in self.grades.items():
+            averaga = sum(values) / len(values)
+        return averaga
+    
+    def __str__(self):
+        res = f'Имя: {self.name}, Фамилия: {self.surname}, Средняя оценка за домашнее задание: {self.avg()}, Курсы в процессе изучения: {self.courses_in_progress}, Завершенные курсы: {self.finished_courses}'
+        return res
+
+    
+
+
+    
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
-        self.grades = {}
 
 
 class Lecturer(Mentor):
@@ -31,12 +43,11 @@ class Lecturer(Mentor):
 
     def average (self):
         for key, sub_dict in self.grades.items():
-            values = sub_dict.values()
-            res = sum(values) / len(values)
+            res = sum(sub_dict) / len(sub_dict)
         return res
 
     def __str__(self):
-        res = f'Имя: {self.name}, Фамилия: {self.surname}, Средняя оценка за лекции: {self.average}'
+        res = f'Имя: {self.name}, Фамилия: {self.surname}, Средняя оценка за лекции: {self.average()}'
         return res
 
 class Reviewer(Mentor):
@@ -66,12 +77,39 @@ class Reviewer(Mentor):
  
 # print(best_student.grades)
 some_reviwer = Reviewer('Some', 'Body')
+first_reviwer = Reviewer('Some', 'Body')
+second_reviwer = Reviewer('Some', 'Body')
+third_reviwer = Reviewer('Some', 'Body')
 some_lecturer = Lecturer('Some', 'One')
 
+#Студенты
 best_student = Student('Shinra', 'Tensei', 'male')
+middle_student = Student('Sakura', 'Uchiha', 'female')
+bad_student = Student('Dzundzi', 'Ito', 'male')
+average_student = Student('Ruoy', 'Eman', 'male')
+
+#Присоединение курсов
+average_student.courses_in_progress += ['Python']
+bad_student.courses_in_progress += ['Python']
+middle_student.courses_in_progress += ['Python']
 best_student.courses_in_progress += ['Python']
 some_lecturer.courses_attached += ['Python']
 
-best_student.rate_lecturer(some_lecturer, 'Python', 8)
+#Пройденные курсы 
+best_student.finished_courses += ['Начало программирования']
 
-print(some_lecturer)
+#Оценки лекторов
+best_student.rate_lecturer(some_lecturer, 'Python', 8)
+middle_student.rate_lecturer(some_lecturer, 'Python', 7.5)
+average_student.rate_lecturer(some_lecturer, 'Python', 9)
+bad_student.rate_lecturer(some_lecturer, 'Python', 4.6)
+
+#Оценки студентов
+some_reviwer.rate_hw(best_student, 'Python', 9)
+first_reviwer.rate_hw(best_student, 'Python', 8.7)
+second_reviwer.rate_hw(best_student, 'Python', 10)
+third_reviwer.rate_hw(best_student, 'Python', 9.7)
+
+
+print(best_student)
+
