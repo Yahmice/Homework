5b7a85b42ae1b4201d6301f592297bb2aff5448b
Yahmice <83534639+Yahmice@users.noreply.github.com>
2023-02-07 18:30:35 +0500

    Update Homework #1.py

--- Homework #1.py
+++ Homework #1.py
@@ -1,21 +1,69 @@
-import requests
-import operator
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+    
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in self.finished_courses:  
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+        
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+        self.grades = {}
 
 
-url = "https://akabab.github.io/superhero-api/api/all.json"
-resp = requests.get(url)
-heroes_list = ['Hulk', 'Captain America', 'Thanos']
-list_of_dictionaries = resp.json()
+class Lecturer(Mentor):
+    def average (self):
+        for key, sub_dict in self.grades.items():
+            values = sub_dict.values()
+            res = sum(values) / len(values)
+        print(res)
 
-intelligence = []
+    def __str__(self):
+        res = f'Имя: {self.name}, Фамилия: {self.surname}, Средняя оценка за лекции: {self.average}'
+        return res
 
+class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+    
+    def __str__(self):
+        res = f'Имя: {self.name}, Фамилия: {self.surname}'
+        return res
+        
+ 
+# best_student = Student('Ruoy', 'Eman', 'your_gender')
+# 
+ 
+# cool_mentor = Mentor('Some', 'Buddy')
+# cool_mentor.courses_attached += ['Python']
+ 
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+ 
+# print(best_student.grades)
+some_reviwer = Reviewer('Some', 'Body')
+some_lecturer = Lecturer('Some', 'Body')
 
-for value in list_of_dictionaries:
-    result = value['powerstats']['intelligence']
-    if value['name'] in heroes_list:
-        intelligence.append(result)
-
-full_heroes_list = dict(zip(heroes_list, intelligence))
-
-maximum = max(full_heroes_list, key = full_heroes_list.get)
-print(maximum)
+best_student = Student('Shinra', 'Tensei', 'male')
+best_student.courses_in_progress += ['Python']
+best_student.rate_lecturer(some_lecturer, 'Python', 9)
